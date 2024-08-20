0b29137e50b62ad73913748681ffcc06a6b4fafd
Vinay RS <vinay.ramesh@appsndevices.in>
2024-08-20 15:54:34 +0530

    Updated the icon urls

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -10,10 +10,26 @@ const NavBar = () => {
         <h1 className="text-6xl ">{"VRS"}</h1>
       </div>
       <div className="m-8 flex items-center justify-center gap-4 text-2xl">
-        <FaLinkedin />
-        <FaGithub />
-        <FaInstagram />
-        <FaSquareXTwitter />
+        <FaLinkedin
+          onClick={() =>
+            window.open(
+              "https://www.linkedin.com/in/vinay-ramesh-soppadal-17855b14b/"
+            )
+          }
+          className="cursor-pointer"
+        />
+        <FaGithub
+          onClick={() => window.open("https://github.com/vinay-ramesh")}
+          className="cursor-pointer"
+        />
+        <FaInstagram
+          onClick={() => window.open("https://www.instagram.com/vinay.r.s")}
+          className="cursor-pointer"
+        />
+        <FaSquareXTwitter
+          onClick={() => window.open("https://x.com/vinayramesh7813")}
+          className="cursor-pointer"
+        />
       </div>
     </nav>
   );
